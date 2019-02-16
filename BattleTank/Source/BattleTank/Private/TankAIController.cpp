3df1075e838d5adc3c468b9fe3e7a465853f87cc
DESKTOP-O2PCDO1\dagermai <dgermain246@gmail.com>
2019-02-16 18:49:42 -0500

    Creating an AI Controller Class

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -3,5 +3,25 @@
 #include "TankAIController.h"
 
 
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController not posessing tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI Controller posessing: %s"), *(ControlledTank->GetName()));
+	}
+
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
 
 
--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Tank.h"
 #include "AIController.h"
 #include "TankAIController.generated.h"
 
@@ -14,7 +15,9 @@ class BATTLETANK_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
 	
-	
-	
+public:
+	ATank * GetControlledTank() const;
+
+	virtual void BeginPlay() override;
 	
 };
