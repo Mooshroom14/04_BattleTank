5cc3689913031a0d19dd1235888268918adc33ef
DESKTOP-O2PCDO1\dagermai <dgermain246@gmail.com>
2019-03-26 16:01:44 -0400

    Using SpawnActor<>() To Spawn

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -10,6 +10,7 @@
 class UTankAimingComponent;
 class UTankBarrel; 
 class UTankTurret;
+class AProjectile;
 
 UCLASS()
 class BATTLETANK_API ATank : public APawn
@@ -43,4 +44,10 @@ private:
 
 	UPROPERTY(EditAnywhere, Category = Firing)
 	float LaunchSpeed = 4000; 
+
+	UPROPERTY(EditAnywhere, Category = Setup)
+	TSubclassOf<AProjectile> ProjectileBlueprint;
+
+	// Local reference to barrel for spawning projectiles
+	UTankBarrel* Barrel = nullptr;
 };
